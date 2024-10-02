cd877b1eb718f1d718ef5fdcc487845f46e4ef9e
lizispretty <chu07200@gmail.com>
2024-10-02 19:56:07 +0900

    Update website.py

--- website.py
+++ website.py
@@ -2,8 +2,4 @@ import streamlit as st
 import pandas as pd
 from streamlit_image_coordinates import streamlit_image_coordinates
 
-value = streamlit_image_coordinates("https://placekitten.com/200/300")
-
-st.write(value)
-
 st.title("Liz")
