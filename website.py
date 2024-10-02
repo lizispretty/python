ec7d869e662b9fab1ded074cdf32848ba3c8232a
lizispretty <chu07200@gmail.com>
2024-10-02 20:17:30 +0900

    Update website.py

--- website.py
+++ website.py
@@ -1,6 +1,6 @@
 import streamlit as st
-import streamlit_image_coordinates as im
+from streamlit_image_coordinates import streamlit_image_coordinates
 
-image = im("C:\\Users\\PC\\Pictures\\리즈\\liz.jpg")
-st.write(image)
+image = streamlit_image_coordinates("https://search.pstatic.net/common/?src=http%3A%2F%2Fimgnews.naver.net%2Fimage%2F109%2F2024%2F07%2F21%2F0005121079_001_20240721174820007.jpg&type=a340")
+st.write
 st.title("Liz")
