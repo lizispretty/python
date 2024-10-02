5c06d369053597d5476e45985462ade2948d7019
lizispretty <chu07200@gmail.com>
2024-10-02 18:57:30 +0900

    Update website.py

--- website.py
+++ website.py
@@ -1,12 +1,4 @@
 import streamlit as st
 import pandas as pd
 
-view = [100, 150, 30]
-st.write('# eating food')
-st.write("## raw")
-view
-
-st.write("## bar chart")
-st.bar_chart(view)
-sview = pd.Series(view)
-sview
+st.title("This is a title")
