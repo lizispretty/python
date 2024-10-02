b4ea922bf24cbb811086ae88fc174a58a5f8347f
lizispretty <chu07200@gmail.com>
2024-10-02 19:08:57 +0900

    Update website.py

--- website.py
+++ website.py
@@ -2,7 +2,8 @@ import streamlit as st
 import pandas as pd
 from streamlit_image_coordinates import streamlit_image_coordinates
 
-st.title("Liz")
-value = streamlit_image_coordinates("https://search.pstatic.net/common/?src=http%3A%2F%2Fimgnews.naver.net%2Fimage%2F433%2F2024%2F09%2F19%2F0000108883_002_20240919100130173.jpg&type=a340")
+value = streamlit_image_coordinates("https://placekitten.com/200/300")
 
 st.write(value)
+
+st.title("Liz")
