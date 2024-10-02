d653775329f2ea563f9026bc81a659b6603d164f
lizispretty <chu07200@gmail.com>
2024-10-02 20:04:54 +0900

    Update website.py

--- website.py
+++ website.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import streamlit_image_coordinates as im
 
-image = im("https://search.naver.com/search.naver?ssc=tab.image.all&where=image&sm=tab_jum&query=%EB%A6%AC%EC%A6%88#imgId=image_sas%3Anews_6fe6169a625961c3bf29ef27e9c2f429")
+image = im("C:\\Users\\PC\\Pictures\\리즈\\liz.jpg")
 st.write(image)
 st.title("Liz")
