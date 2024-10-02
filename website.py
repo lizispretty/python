0366573690532261c4f5a39491b1f084e33807df
lizispretty <chu07200@gmail.com>
2024-10-02 20:18:50 +0900

    Update website.py

--- website.py
+++ website.py
@@ -1,7 +1,4 @@
 import streamlit as st
 from streamlit_image_coordinates import streamlit_image_coordinates
 
-image = streamlit_image_coordinates("https://search.pstatic.net/common/?src=http%3A%2F%2Fimgnews.naver.net%2Fimage%2F109%2F2024%2F07%2F21%2F0005121079_001_20240721174820007.jpg&type=a340")
-st.write
-
 st.title("Liz")
