522cf02740174cbd882c04d43610116784826509
lizispretty <chu07200@gmail.com>
2024-10-03 12:38:12 +0900

    Update website.py

--- website.py
+++ website.py
@@ -4,3 +4,5 @@ image_url1 = "https://imgnews.pstatic.net/image/433/2024/09/19/0000108883_001_20
 st.image(image_url1)
 image_url2 = "https://imgnews.pstatic.net/image/001/2023/12/02/PYH2023120203560001300_P4_20231202163507885.jpg?type=w647"
 st.image(image_url2)
+image_url3 = "https://postfiles.pstatic.net/MjAyNDEwMDNfNDUg/MDAxNzI3OTI1NTExNzA5.F6Q22hCLp26kuknWQ5DQkcq99ZNZ_jGtmJJJGOApCv4g.yOKMK2hnWp06T8Z7VxRG51sRiIFv48tdo9jxhC4OYNUg.JPEG/liz1.jpg?type=w966"
+st.image(image_url3)
