f19d10848d0243f559241711cb2f239ff093baee
lizispretty <chu07200@gmail.com>
2024-10-03 12:43:53 +0900

    Update website.py

--- website.py
+++ website.py
@@ -3,12 +3,7 @@ import streamlit as st
 st.title("Liz is pretty")
 st.button("Reset", type="primary")
 if st.button("Say hello"):
-    st.write("Why hello there")
+    image_url1 = "https://imgnews.pstatic.net/image/433/2024/09/19/0000108883_001_20240919100130136.jpg?type=w647"
+    st.image(image_url1)
 else:
     st.write("Goodbye")
-             
-
-image_url1 = "https://imgnews.pstatic.net/image/433/2024/09/19/0000108883_001_20240919100130136.jpg?type=w647"
-st.image(image_url1)
-image_url2 = "https://imgnews.pstatic.net/image/001/2023/12/02/PYH2023120203560001300_P4_20231202163507885.jpg?type=w647"
-st.image(image_url2)
