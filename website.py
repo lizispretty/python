0b10fba93fea52a833dfafd3adaeb0c3a6957558
lizispretty <chu07200@gmail.com>
2024-10-03 12:46:08 +0900

    Update website.py

--- website.py
+++ website.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("Liz is pretty")
+st.title("Liz is pretty"), st.title("liz is pretty")
 if st.button("Wathing anything"):
     image_url1 = "https://imgnews.pstatic.net/image/433/2024/09/19/0000108883_001_20240919100130136.jpg?type=w647"
     st.image(image_url1)
