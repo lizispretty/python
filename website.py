731eed7fd7addc31caf2acdc834674c1952c2d4a
lizispretty <chu07200@gmail.com>
2024-10-03 12:22:03 +0900

    Update website.py

--- website.py
+++ website.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 image_url = "https://imgnews.pstatic.net/image/433/2024/09/19/0000108883_001_20240919100130136.jpg?type=w647"
 
-st.image(image_url, caption= 'streamlit blog image', use_column_width= True)
+st.image(image_url, caption= '출처: H@포토')
