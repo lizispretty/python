8c4650209b911d484ccc0dcfa70d57449cd6a678
lizispretty <chu07200@gmail.com>
2024-10-03 12:46:59 +0900

    Update website.py

--- website.py
+++ website.py
@@ -5,4 +5,4 @@ if st.button("Wathing anything"):
     image_url1 = "https://imgnews.pstatic.net/image/433/2024/09/19/0000108883_001_20240919100130136.jpg?type=w647"
     st.image(image_url1)
 else:
-    st.write("Goodbye")
+    st.write("")
