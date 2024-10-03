c58d10a433cb9a15ec4fb3ac3c174e0c96777282
lizispretty <chu07200@gmail.com>
2024-10-03 15:32:47 +0900

    Update website.py

--- website.py
+++ website.py
@@ -1,4 +1,12 @@
 import streamlit as st
 
-title = st.text_input("Movie title", "Life of Brian")
-st.write("The current movie title is", title)
+txt = st.text_area(
+    "Text to analyze",
+    "It was the best of times, it was the worst of times, it was the age of "
+    "wisdom, it was the age of foolishness, it was the epoch of belief, it "
+    "was the epoch of incredulity, it was the season of Light, it was the "
+    "season of Darkness, it was the spring of hope, it was the winter of "
+    "despair, (...)",
+)
+
+st.write(f"You wrote {len(txt)} characters.")
