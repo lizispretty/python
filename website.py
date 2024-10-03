de26f4d93ea2e904ec67fe9e333177ae26afb3a3
lizispretty <chu07200@gmail.com>
2024-10-03 12:19:54 +0900

    Update website.py

--- website.py
+++ website.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-image_url = "https://postfiles.pstatic.net/MjAyNDEwMDNfMjE0/MDAxNzI3OTI0OTQ2Mzky.ixi1RapZR1UHBSg5UcFujyRD-QBAQdmmAHHmzO6PbXwg.hnPr0lWLlI4yrWkTbbB4dcjHSIlErv3iAASXsOSumSsg.JPEG/liz.jpg?type=w966"
+image_url = "https://postfiles.pstatic.net/MjAyNDEwMDNfNDUg/MDAxNzI3OTI1NTExNzA5.F6Q22hCLp26kuknWQ5DQkcq99ZNZ_jGtmJJJGOApCv4g.yOKMK2hnWp06T8Z7VxRG51sRiIFv48tdo9jxhC4OYNUg.JPEG/liz1.jpg?type=w966"
 
-st.image(image_url)
+st.image(image_url, caption= 'streamlit blog image', use_column_width= True)
