9d5105fa9ded109b25933532aaf669cfe411b09e
lizispretty <chu07200@gmail.com>
2024-10-03 12:15:41 +0900

    Update website.py

--- website.py
+++ website.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 image_url = "https://postfiles.pstatic.net/MjAyNDEwMDNfMjE0/MDAxNzI3OTI0OTQ2Mzky.ixi1RapZR1UHBSg5UcFujyRD-QBAQdmmAHHmzO6PbXwg.hnPr0lWLlI4yrWkTbbB4dcjHSIlErv3iAASXsOSumSsg.JPEG/liz.jpg?type=w966"
 
-st.image(image_url, caption= 'streamlit blog image', use_column_width= True)
+st.image(image_url)
