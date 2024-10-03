0f7b515d5d27c8ed6e3e8fa0766855a66e5af28e
lizispretty <chu07200@gmail.com>
2024-10-03 12:39:51 +0900

    Update website.py

--- website.py
+++ website.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("lizis pretty")
+st.title("Liz is pretty")
 image_url1 = "https://imgnews.pstatic.net/image/433/2024/09/19/0000108883_001_20240919100130136.jpg?type=w647"
 st.image(image_url1)
 image_url2 = "https://imgnews.pstatic.net/image/001/2023/12/02/PYH2023120203560001300_P4_20231202163507885.jpg?type=w647"
