875d834b3e1fe9a22b56f0f9ca96a6e6778cb58d
lizispretty <chu07200@gmail.com>
2024-10-03 12:12:53 +0900

    Update website.py

--- website.py
+++ website.py
@@ -1,6 +1,5 @@
 import streamlit as st
 
-from PIL import Image
+image_url = "https://postfiles.pstatic.net/MjAyNDEwMDNfMjE0/MDAxNzI3OTI0OTQ2Mzky.ixi1RapZR1UHBSg5UcFujyRD-QBAQdmmAHHmzO6PbXwg.hnPr0lWLlI4yrWkTbbB4dcjHSIlErv3iAASXsOSumSsg.JPEG/liz.jpg?type=w966"
 
-img = Image.open('C:/Users/PC/Pictures/liz/liz.jpg')
-st.image(img)
+st.Image(image_url, caption= 'streamlit blog image', use_column_width= True)
