35a1799896c7dc91bccc28886407a1cb2032a993
lizispretty <chu07200@gmail.com>
2024-10-03 20:26:36 +0900

    Update website.py

--- website.py
+++ website.py
@@ -1,12 +1,7 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
-txt = st.text_area(
-    "Text to analyze",
-    "It was the best of times, it was the worst of times, it was the age of "
-    "wisdom, it was the age of foolishness, it was the epoch of belief, it "
-    "was the epoch of incredulity, it was the season of Light, it was the "
-    "season of Darkness, it was the spring of hope, it was the winter of "
-    "despair, (...)",
-)
+chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
 
-st.write(f"You wrote {len(txt)} characters.")
+st.line_chart(chart_data)
